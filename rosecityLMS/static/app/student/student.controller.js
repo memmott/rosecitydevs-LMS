3b434a27860505564c38457bc23eb28fdd932b69
Brigette Eckert <brigette.eckert@live.com>
2016-02-13 11:37:39 -0800

    fixed some path errors causing bugs and added mock data file and http request

--- rosecityLMS/static/app/student/student.controller.js
+++ rosecityLMS/static/app/student/student.controller.js
@@ -1,12 +1,17 @@
 angular.module('StudentApp', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-	$routeProvider.when('/', {
-		controller:'studentCtrl',
-		templateUrl:'views/student.html'
-	})
-	
-.controller('MainController', function($scope) {
+    $routeProvider.when('/', {
+        controller:'StudentCtrl',
+        templateUrl:'views/student.html'
+    })
+}])
+    
+.controller('StudentCtrl', ['$scope', '$http', function($scope, $http) {
+    $http.get('mock/student.json').success(function(data){
+        console.log(data);
+    }).error(function(err){
+        console.log(err);
+    });
 
-
-});
+}]);
