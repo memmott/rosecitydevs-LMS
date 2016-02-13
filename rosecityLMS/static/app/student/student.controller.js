1af7cc5cf8a12291d79f5bf63cc76bc977de3480
Brigette Eckert <brigette.eckert@live.com>
2016-02-13 12:22:17 -0800

    changed student view page to use student mock json data templating

--- rosecityLMS/static/app/student/student.controller.js
+++ rosecityLMS/static/app/student/student.controller.js
@@ -10,6 +10,8 @@ angular.module('StudentApp', ['ngRoute'])
 .controller('StudentCtrl', ['$scope', '$http', function($scope, $http) {
     $http.get('mock/student.json').success(function(data){
         $scope.students = data;
+        $scope.courses = data.courses;
+        console.log($scope.courses)
         console.log(data);
     }).error(function(err){
         console.log(err);
